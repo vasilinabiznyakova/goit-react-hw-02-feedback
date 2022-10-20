cc14bee4d768f6685ac36d9364ef8dc234ad5cfb
Vasilina Biznyakova <vbiznyakova@gmail.com>
2022-10-20 17:59:32 +0200

    fix prop on notification component

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -2,6 +2,6 @@ import PropTypes from 'prop-types';
 
 export const Notification = ({ message }) => <p>{message}</p>;
 
-Notification.propTypes = PropTypes.objectOf(
-  PropTypes.string.isRequired
-).isRequired;
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+};
